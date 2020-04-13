6c377d8c7b789320deccd2d84888fb367678e334
aselma78 <andres.selma78@gmail.com>
2020-04-13 22:11:04 +0200

    Fixed set_value deprecated function

--- Indicators.py
+++ Indicators.py
@@ -74,7 +74,7 @@ def acc_dist(data, trend_periods=21, open_col='open', high_col='high', low_col='
             ac = ((row[close_col] - row[low_col]) - (row[high_col] - row[close_col])) / (row[high_col] - row[low_col]) * row[vol_col]
         else:
             ac = 0
-        data.set_value(index, 'acc_dist', ac)
+        data.at[index, 'acc_dist'] = ac
     data['acc_dist_ema' + str(trend_periods)] = data['acc_dist'].ewm(ignore_na=False, min_periods=0, com=trend_periods, adjust=True).mean()
     
     return data
@@ -105,7 +105,7 @@ def on_balance_volume(data, trend_periods=21, close_col='close', vol_col='volume
             last_obv = 0
             current_obv = row[vol_col]
 
-        data.set_value(index, 'obv', current_obv)
+        data.at[index, 'obv'] = current_obv
 
     data['obv_ema' + str(trend_periods)] = data['obv'].ewm(ignore_na=False, min_periods=0, com=trend_periods, adjust=True).mean()
     
@@ -134,7 +134,7 @@ def price_volume_trend(data, trend_periods=21, close_col='close', vol_col='volum
         else:
             current_val = row[vol_col]
 
-        data.set_value(index, 'pvt', current_val)
+        data.at[index, 'pvt'] = current_val
 
     data['pvt_ema' + str(trend_periods)] = data['pvt'].ewm(ignore_na=False, min_periods=0, com=trend_periods, adjust=True).mean()
         
@@ -168,7 +168,7 @@ def average_true_range(data, trend_periods=14, open_col='open', high_col='high',
         else:
             true_range = np.amax(prices) - np.amin(prices)
 
-        data.set_value(index, 'true_range', true_range)
+        data.at[index, 'true_range'] = true_range
     data['atr'] = data['true_range'].ewm(ignore_na=False, min_periods=0, com=trend_periods, adjust=True).mean()
     if drop_tr:
         data = data.drop(['true_range'], axis=1)
@@ -202,8 +202,8 @@ def bollinger_bands(data, trend_periods=20, close_col='close'):
         upper_band = middle_band + (d * std)
         lower_band = middle_band - (d * std)
 
-        data.set_value(index, 'bol_bands_upper', upper_band)
-        data.set_value(index, 'bol_bands_lower', lower_band)
+        data.at[index, 'bol_bands_upper'] = upper_band
+        data.at[index, 'bol_bands_lower'] = lower_band
 
     return data
 
@@ -291,7 +291,7 @@ def ease_of_movement(data, period=14, high_col='high', low_col='low', vol_col='v
         box_ratio = (vol / 100000000) / (diff)
         emv = midpoint_move / box_ratio
         
-        data.set_value(index, 'emv', emv)
+        data.at[index, 'emv'] = emv
         
     data['emv_ema_'+str(period)] = data['emv'].ewm(ignore_na=False, min_periods=0, com=period, adjust=True).mean()
         
@@ -320,7 +320,7 @@ def mass_index(data, period=25, ema_period=9, high_col='high', low_col='low'):
             val = div[index-25:index].sum()
         else:
             val = 0
-        data.set_value(index, 'mass_index', val)
+        data.at[index, 'mass_index'] = val
          
     return data
 
@@ -347,24 +347,24 @@ def directional_movement_index(data, periods=14, high_col='high', low_col='low')
     
     for i,row in data.iterrows():
         if i>0:
-            data.set_value(i, 'm_plus', row[high_col] - data.at[i-1, high_col])
-            data.set_value(i, 'm_minus', row[low_col] - data.at[i-1, low_col])
+            data.at[i, 'm_plus'] = row[high_col] - data.at[i-1, high_col]
+            data.at[i, 'm_minus'] = row[low_col] - data.at[i-1, low_col]
     
     data['dm_plus'] = 0.
     data['dm_minus'] = 0.
     
     for i,row in data.iterrows():
         if row['m_plus'] > row['m_minus'] and row['m_plus'] > 0:
-            data.set_value(i, 'dm_plus', row['m_plus'])
+            data.at[i, 'dm_plus'] = row['m_plus']
             
         if row['m_minus'] > row['m_plus'] and row['m_minus'] > 0:
-            data.set_value(i, 'dm_minus', row['m_minus'])
+            data.at[i, 'dm_minus'] = row['m_minus']
     
     data['di_plus'] = (data['dm_plus'] / data['true_range']).ewm(ignore_na=False, min_periods=0, com=periods, adjust=True).mean()
     data['di_minus'] = (data['dm_minus'] / data['true_range']).ewm(ignore_na=False, min_periods=0, com=periods, adjust=True).mean()
     
     data['dxi'] = np.abs(data['di_plus'] - data['di_minus']) / (data['di_plus'] + data['di_minus'])
-    data.set_value(0, 'dxi',1.)
+    data.at[0, 'dxi'] =1.
     data['adx'] = data['dxi'].ewm(ignore_na=False, min_periods=0, com=periods, adjust=True).mean()
     data = data.drop(['m_plus', 'm_minus', 'dm_plus', 'dm_minus'], axis=1)
     if remove_tr_col:
@@ -449,7 +449,7 @@ def negative_volume_index(data, periods=255, close_col='close', vol_col='volume'
                 nvi = prev_nvi
         else:
             nvi = 1000
-        data.set_value(index, 'nvi', nvi)
+        data.at[index, 'nvi'] = nvi
     data['nvi_ema'] = data['nvi'].ewm(ignore_na=False, min_periods=0, com=periods, adjust=True).mean()
     
     return data
@@ -479,7 +479,7 @@ def positive_volume_index(data, periods=255, close_col='close', vol_col='volume'
                 pvi = prev_pvi
         else:
             pvi = 1000
-        data.set_value(index, 'pvi', pvi)
+        data.at[index, 'pvi'] = pvi
     data['pvi_ema'] = data['pvi'].ewm(ignore_na=False, min_periods=0, com=periods, adjust=True).mean()
 
     return data
@@ -503,7 +503,7 @@ def momentum(data, periods=14, close_col='close'):
             prev_close = data.at[index-periods, close_col]
             val_perc = (row[close_col] - prev_close)/prev_close
 
-            data.set_value(index, 'momentum', val_perc)
+            data.at[index, 'momentum'] = val_perc)
 
     return data
 
@@ -528,9 +528,9 @@ def rsi(data, periods=14, close_col='close'):
             
             prev_close = data.at[index-periods, close_col]
             if prev_close < row[close_col]:
-                data.set_value(index, 'rsi_u', row[close_col] - prev_close)
+                data.at[index, 'rsi_u'] = row[close_col] - prev_close
             elif prev_close > row[close_col]:
-                data.set_value(index, 'rsi_d', prev_close - row[close_col])
+                data.at[index, 'rsi_d'] = prev_close - row[close_col]
             
     data['rsi'] = data['rsi_u'].ewm(ignore_na=False, min_periods=0, com=periods, adjust=True).mean() / (data['rsi_u'].ewm(ignore_na=False, min_periods=0, com=periods, adjust=True).mean() + data['rsi_d'].ewm(ignore_na=False, min_periods=0, com=periods, adjust=True).mean())
     
@@ -564,7 +564,7 @@ def chaikin_volatility(data, ema_periods=10, change_periods=10, high_col='high',
             if prev_value == 0:
                 #this is to avoid division by zero below
                 prev_value = 0.0001
-            data.set_value(index, 'chaikin_volatility', ((row['ch_vol_ema'] - prev_value)/prev_value))
+            data.at[index, 'chaikin_volatility'] = (row['ch_vol_ema'] - prev_value)/prev_value
             
     data = data.drop(['ch_vol_hl', 'ch_vol_ema'], axis=1)
         
@@ -596,7 +596,7 @@ def williams_ad(data, high_col='high', low_col='low', close_col='close'):
             else:
                 ad = 0.
                                                                                                         
-            data.set_value(index, 'williams_ad', (ad+prev_value))
+            data.at[index, 'williams_ad'] = ad+prev_value
         
     return data
 
@@ -618,8 +618,8 @@ def williams_r(data, periods=14, high_col='high', low_col='low', close_col='clos
     
     for index,row in data.iterrows():
         if index > periods:
-            data.set_value(index, 'williams_r', ((max(data[high_col][index-periods:index]) - row[close_col]) / 
-                                                 (max(data[high_col][index-periods:index]) - min(data[low_col][index-periods:index]))))
+            data.at[index, 'williams_r'] = ((max(data[high_col][index-periods:index]) - row[close_col]) / 
+                                                 (max(data[high_col][index-periods:index]) - min(data[low_col][index-periods:index])))
         
     return data
 
@@ -672,19 +672,19 @@ def ultimate_oscillator(data, period_1=7,period_2=14, period_3=28, high_col='hig
             bp = row[close_col] - min(row[low_col], data.at[index-1, close_col])
             tr = max(row[high_col], data.at[index-1, close_col]) - min(row[low_col], data.at[index-1, close_col])
             
-            data.set_value(index, 'uo_bp', bp)
-            data.set_value(index, 'uo_tr', tr)
+            data.at[index, 'uo_bp'] = bp
+            data.at[index, 'uo_tr'] = tr
             if index >= period_1:
                 uo_avg_1 = sum(data['uo_bp'][index-period_1:index]) / sum(data['uo_tr'][index-period_1:index])
-                data.set_value(index, 'uo_avg_1', uo_avg_1)
+                data.at[index, 'uo_avg_1'] = uo_avg_1
             if index >= period_2:
                 uo_avg_2 = sum(data['uo_bp'][index-period_2:index]) / sum(data['uo_tr'][index-period_2:index])
-                data.set_value(index, 'uo_avg_2', uo_avg_2)
+                data.at[index, 'uo_avg_2'] = uo_avg_2
             if index >= period_3:
                 uo_avg_3 = sum(data['uo_bp'][index-period_3:index]) / sum(data['uo_tr'][index-period_3:index])
-                data.set_value(index, 'uo_avg_3', uo_avg_3)
+                data.at[index, 'uo_avg_3'] = uo_avg_3
                 uo = (4 * uo_avg_1 + 2 * uo_avg_2 + uo_avg_3) / 7
-                data.set_value(index, 'ultimate_oscillator', uo)
+                data.at[index, 'ultimate_oscillator'] = uo
 
     data = data.drop(['uo_bp', 'uo_tr', 'uo_avg_1', 'uo_avg_2', 'uo_avg_3'], axis=1)
         
